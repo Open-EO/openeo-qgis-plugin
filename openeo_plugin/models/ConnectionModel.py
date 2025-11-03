6b1761f021379e3c3eff9bd2b2cf418d61578939
niebl <caro.niebl@uni-muenster.de>
2025-11-03 08:57:13 +0100

    fix: remove unnecessary use of eval

--- openeo_plugin/models/ConnectionModel.py
+++ openeo_plugin/models/ConnectionModel.py
@@ -11,8 +11,8 @@ class ConnectionModel():
         # alternative constructor from json
         data = json.loads(json_string)
 
-        name = b64decode(eval(data["name"])).decode("utf-8")
-        url = b64decode(eval(data["url"])).decode("utf-8")
+        name = b64decode(data["name"]).decode("utf-8")
+        url = b64decode(data["url"]).decode("utf-8")
         
         return cls(name=name, url=url)
 
@@ -21,8 +21,8 @@ class ConnectionModel():
     
     def __str__(self):
         dict = {
-            "name": str(b64encode(self.name.encode("utf-8"))),
-            "url": str(b64encode(self.url.encode("utf-8")))
+            "name": b64encode(self.name.encode("utf-8")).decode("utf-8"),
+            "url": b64encode(self.url.encode("utf-8")).decode("utf-8")
         }
         
         return json.dumps(dict)

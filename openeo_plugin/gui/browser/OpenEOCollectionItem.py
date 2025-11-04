401923f8de9a322cd0b85ac098f4621ca5d0b036
niebl <caro.niebl@uni-muenster.de>
2025-11-04 10:54:27 +0100

    wip: implement getCapabilities scanning. slow.

--- openeo_plugin/gui/browser/OpenEOCollectionItem.py
+++ openeo_plugin/gui/browser/OpenEOCollectionItem.py
@@ -1,9 +1,13 @@
+from qgis.PyQt.QtGui import QIcon
+from qgis.PyQt.QtWidgets import QAction
+
 from qgis.core import Qgis
 from qgis.core import QgsLayerItem
 from qgis.core import QgsDataCollectionItem
 from qgis.core import QgsDataItem
 
 from urllib.parse import urlencode, unquote, quote
+from owslib.wmts import WebMapTileService
 
 
 class OpenEOCollectionItem(QgsDataCollectionItem):
@@ -50,12 +54,31 @@ class OpenEOCollectionLayerItem(QgsLayerItem):
         """
         uri = None
 
-        webMapLink = self.parent().getWebMapLinks(collection)[0]
+        webMapLink = parent.getWebMapLinks(collection)[0]
         # different map service formats
         if webMapLink["rel"] == "xyz":
             uri = f"type=xyz&url={webMapLink["href"]}/"+quote("{z}/{y}/{x}") 
             # example
             # uri= "type=xyz&url=https://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/tile/"+quote("{z}/{y}/{x}")
+
+        if webMapLink["rel"] == "wmts":
+            wmtsUrl = webMapLink["href"]+"?service=wmts&request=getCapabilities"
+            wmts = WebMapTileService(wmtsUrl)
+            targetCRS = "EPSG::3857"
+            
+            tileMatrixSet = None
+            for tms_id, tms in list(wmts.tilematrixsets.items()):
+                if targetCRS in tms.crs: 
+                    tileMatrixSet = tms_id
+                    break
+            layerID = None
+            layerID = list(wmts.contents)[0]
+            styleID = wmts.contents[layerID].styles
+
+            
+            
+            uri = f"crs=EPSG:3857&styles=default&tilePixelRatio=0&format=image/png&layers={layerID}&tileMatrixSet={tileMatrixSet}&url={webMapLink["href"]}"
+
 
         QgsLayerItem.__init__(
             self,
@@ -72,3 +95,16 @@ class OpenEOCollectionLayerItem(QgsLayerItem):
 
     def getConnection(self):
         return self.parent().getConnection()
+    
+    def getMime(self):
+        # mimeUri is deprecated.
+        print(self.mimeUri())
+    
+    def actions(self, parent):
+        action_mime = QAction(QIcon(), "get mime uri", parent)
+        action_mime.triggered.connect(self.getMime)
+        actions = [action_mime]
+        return actions
+
+
+

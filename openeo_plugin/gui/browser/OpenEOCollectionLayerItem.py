8758bdaa856416da1343ec0d1abd2e63e7a2ebd7
niebl <caro.niebl@uni-muenster.de>
2025-11-10 13:59:56 +0100

    refactor: reimplement OpenEOCollectionLayerItem as subclass of DataItem for more flexibility

--- openeo_plugin/gui/browser/OpenEOCollectionLayerItem.py
+++ openeo_plugin/gui/browser/OpenEOCollectionLayerItem.py
@@ -2,47 +2,60 @@ from urllib.parse import quote
 from ...utils.wmts import WebMapTileService
 from ...utils.logging import warning
 
+from qgis.PyQt.QtGui import QIcon
+from qgis.PyQt.QtWidgets import QAction
+
+from qgis.core import QgsIconUtils
+from qgis.core import QgsCoordinateReferenceSystem
+from qgis.core import Qgis
 from qgis.PyQt.QtCore import Qt
 from qgis.PyQt.QtWidgets import QApplication
 
 from qgis.core import Qgis, QgsLayerItem, QgsDataItem, QgsMimeDataUtils, QgsMapLayerFactory
 
-class OpenEOCollectionLayerItem(QgsLayerItem):
+class OpenEOCollectionLayerItem(QgsDataItem):
     def __init__(self, parent, collection, plugin):
-        """Constructor.
-
-        :param parent: the parent DataItem. expected to be an OpenEOCollectionsGroupItem.
-        :type parent: QgsDataItem
-
-        :param plugin: Reference to the qgis plugin object. Passing this object
-            to the children allows for access to important attributes like
-            PLUGIN_NAME and PLUGIN_ENTRY_NAME.
-        
-        :param collection: dict containing relevant infos about the collection.
-        :type url: dict
-        """
-
-        QgsLayerItem.__init__(
+        name = collection.get("title") or collection.get("id")
+        QgsDataItem.__init__(
             self,
+            type = Qgis.BrowserItemType.Custom,
             parent = parent,
-            name = collection.get("title") or collection.get("id"),
+            name = name,
             path = None,
-            uri = "",
-            layerType = Qgis.BrowserLayerType.Raster,
-            providerKey = "wms"
+            providerKey = plugin.PLUGIN_ENTRY_NAME
         )
+
         self.collection = collection
         self.plugin = plugin
+
         # Has no children, set as populated to avoid the expand arrow
         self.setState(QgsDataItem.Populated)
+
+    def icon(self):
+        return QgsIconUtils.iconRaster()
+
+    def hasDragEnabled(self):
+        return True
+    
+    def layerName(self):
+        return self.name()
+    
+    def supportedFormats(self):
+        return [] #TODO: determine more closely from capabilities
+    
+    def supportedCrs(self):
+        return ["EPSG:3857"] #TODO: determine more closely from capabilities
+    
+    def getConnection(self):
+        return self.parent().getConnection()
 
     def createUri(self, link):
         title = link.get("title") or ""
         rel = link.get("rel") or ""
 
         uri = QgsMimeDataUtils.Uri()
-        uri.layerType = QgsMapLayerFactory.typeToString(self.mapLayerType())
-        uri.providerKey = self.providerKey()
+        uri.layerType = QgsMapLayerFactory.typeToString(Qgis.LayerType.Raster)  
+        uri.providerKey = "wms"
         uri.name = self.layerName()
         if len(title) > 0 and title != uri.name:
             uri.name += f" - {title}"
@@ -72,14 +85,13 @@ class OpenEOCollectionLayerItem(QgsLayerItem):
             return uri
         else:
             return None
-
-    def getConnection(self):
-        return self.parent().getConnection()
-
+        
+    
     def mimeUris(self):
         mimeUris = []
 
         webMapLinks = self.parent().getWebMapLinks(self.collection)
+        print(webMapLinks)
         if len(webMapLinks) == 0:
             warning(self.plugin.iface, "Could not detect a layer from the given source.")
             return mimeUris
@@ -95,17 +107,19 @@ class OpenEOCollectionLayerItem(QgsLayerItem):
                 print(e)
                 warning(
                     self.plugin.iface,
-                    f"Loading the map service {webMapLinks['href']} failed."
+                    f"Loading the map service {link['href']} failed."
                 )
         
         QApplication.restoreOverrideCursor()
 
         return mimeUris
-         
     
-    #try just returning the new uri through mimeuri
-    
+    def addToProject(self):
+        for uri in self.mimeUris():
+            self.plugin.iface.addRasterLayer(uri.uri, uri.name, uri.providerKey)
+
     def actions(self, parent):
+        action_add_to_project = QAction(QIcon(), "Add Layer to Project", parent)
+        action_add_to_project.triggered.connect(self.addToProject)
+        return [action_add_to_project]
         return []
-
-

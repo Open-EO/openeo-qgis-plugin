408355f56414014e4db118cdcdc22caacbfa44b8
niebl <caro.niebl@uni-muenster.de>
2025-12-15 16:20:19 +0100

    refactor: fetch auth providers before opening login dialog

--- openeo_plugin/gui/browser/OpenEOConnectionItem.py
+++ openeo_plugin/gui/browser/OpenEOConnectionItem.py
@@ -10,7 +10,6 @@ import datetime
 
 from qgis.PyQt.QtGui import QIcon
 from qgis.PyQt.QtWidgets import QAction
-from qgis.PyQt.QtCore import Qt
 from qgis.PyQt.QtWidgets import QApplication
 
 from qgis.core import QgsApplication
@@ -103,19 +102,27 @@ class OpenEOConnectionItem(QgsDataCollectionItem):
             return
 
         try:
-            QApplication.setOverrideCursor(Qt.CursorShape.WaitCursor)
-            self.loginStarted = True
-            try:
-                self.dlg = LoginDialog(
-                    self.plugin, self.getConnection(), model=self.model
-                )
-            except Exception as e:
-                self.plugin.logging.error("Can't open login dialog.", error=e)
-                return
-            finally:
-                QApplication.restoreOverrideCursor()
-                self.loginStarted = False
+            if hasattr(self.connection, "list_auth_providers"):
+                auth_provider_list = self.connection.list_auth_providers()
+            else:
+                # todo: remove this when the openEO Python client has been updated to support this method
+                auth_provider_list = self.list_auth_providers()
+        except Exception as e:
+            self.plugin.logging.error(
+                "Can't open login dialog. Unable to fetch auth providers.",
+                error=e,
+            )
+            return
 
+        try:
+            self.loginStarted = True
+            self.dlg = LoginDialog(
+                self.plugin,
+                self.getConnection(),
+                model=self.model,
+                auth_providers=auth_provider_list,
+            )
+            QApplication.restoreOverrideCursor()
             result = self.dlg.exec()
 
             if result:
@@ -169,6 +176,42 @@ class OpenEOConnectionItem(QgsDataCollectionItem):
                     "Can't check authentication status.", error=e
                 )
         return self.authenticated
+
+    # todo: remove this when the openEO Python client has been updated to support this method
+    def list_auth_providers(self) -> list[dict]:
+        providers = []
+        cap = self.connection.capabilities()
+
+        # Add OIDC providers
+        oidc_path = "/credentials/oidc"
+        if cap.supports_endpoint(oidc_path, method="GET"):
+            try:
+                data = self.connection.get(
+                    oidc_path, expected_status=200
+                ).json()
+                if isinstance(data, dict):
+                    for provider in data.get("providers", []):
+                        provider["type"] = "oidc"
+                        providers.append(provider)
+            except openeo.rest.OpenEoApiError as e:
+                self.plugin.logging.error(
+                    "Can't load the OpenID Connect provider list.", error=e
+                )
+
+        # Add Basic provider
+        basic_path = "/credentials/basic"
+        if cap.supports_endpoint(basic_path, method="GET"):
+            providers.append(
+                {
+                    "id": basic_path,
+                    "issuer": self.connection.build_url(basic_path),
+                    "type": "basic",
+                    "title": "Internal",
+                    "description": "The HTTP Basic authentication method is mostly used for development and testing purposes.",
+                }
+            )
+
+        return providers
 
     def getConnection(self):
         if not self.connection:
